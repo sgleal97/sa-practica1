4fc7c44b64a2c29a1bb0d144e86af260f7a308fa
sgleal97 <sgleal97@gmail.com>
2020-08-02 05:39:49 -0600

    Creacion de proyecto 2.0

--- ht1/ht1/Program.cs
+++ ht1/ht1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ht1
 {
@@ -6,7 +10,6 @@ namespace ht1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
