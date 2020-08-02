5908c9258c0ef5a89041f469963b2f967948a45b
sgleal97 <sgleal97@gmail.com>
2020-08-02 15:58:34 -0600

    Listar Contactos

--- ht1/ht1/Program.cs
+++ ht1/ht1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ht1
 {
@@ -10,6 +11,16 @@ namespace ht1
     {
         static void Main(string[] args)
         {
+            webservice.administratorcontact100Client WS = new webservice.administratorcontact100Client();
+            var Contactos = WS.readList(0,0,null,null,null,null,null);
+            int i = 0;
+            Console.WriteLine("Tamanio:" + Contactos.Length);
+            while (i<Contactos.Length) {
+                Console.WriteLine("Contacto: " + Contactos[i].name);
+                i++;
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
         }
     }
 }
